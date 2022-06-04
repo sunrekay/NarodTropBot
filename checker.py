15d864917d15a0fc3ce4bfd73592e98aef02818f
Студент <student@kafvt.iit.mirea.ru>
2022-06-04 09:35:47 +0300

    add json_database

--- checker.py
+++ checker.py
@@ -1,55 +1,26 @@
 import requests
-from config_bd import host, user, password, db_name
+import database
 from url_bot_getter import get_website_url
-import pymysql
 
 
 def url_data_check(name_website):
-
-    if "https://" in name_website:
-        response = requests.get(name_website)
-    else:
-        response = requests.get(f'https://{name_website}')
-
-    if response.status_code == 200:
-
-        temp = check_database(name_website)
-
-        if temp == 0:
-            return get_website_url(name_website)
-        else:
-            return temp
-    else:
-        return "Нет возможности получить доступ к сайту. Проверьте введенные данные."
-
-
-def check_database(name_website):
     try:
-        connection = pymysql.connect(
-            host=host,
-            port=3306,
-            user=user,
-            password=password,
-            database=db_name,
-            cursorclass=pymysql.cursors.DictCursor
-        )
-        print("Подключение выполнено успешно...")
-        try:
-            with connection.cursor() as cursor:
-                select_all_id = "SELECT url FROM urls_list;"  # Ра
-                cursor.execute(select_all_id)
-                rows = cursor.fetchall()
-                for row in rows:
-                    if row['url'] == name_website:
-                        return row['workaround_url']
-            return 0
-        except Exception as ex:
-            print(ex)
-        finally:
-            connection.close()
+        if "https://" in name_website:
+            response = requests.get(name_website)
+        else:
+            name_website = f'https://{name_website}'
+            response = requests.get(name_website)
+
+        if response.status_code == 200:
+
+            temp = database.check_url_table(name_website)
+
+            if temp == 0:
+                return get_website_url(name_website)
+            else:
+                return temp
+        else:
+            return "Нет возможности получить доступ к сайту. Проверьте введенные данные."
     except Exception as ex:
         print(ex)
-        print("Не удалось подключиться к БД...")
-
-
-print(url_data_check("vk.com"))
+        return "Нет возможности получить доступ к сайту. Проверьте введенные данные."
--- url_bot_getter.py
+++ url_bot_getter.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.firefox import GeckoDriverManager
+import database
 import time
 import os
 
-os.environ['GH_TOKEN'] = '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'
+os.environ['GH_TOKEN'] = ' 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 '
 options = webdriver.FirefoxOptions()
 options.set_preference("dom.webdriver.enabled", False)
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0")
-options.headless = False
+options.headless = True
 
 driver = webdriver.Firefox(
     executable_path=GeckoDriverManager().install(),
@@ -22,7 +23,8 @@ def get_website_url(name_website):
         element = driver.find_element(By.ID, 'appendedInputButton')
         element.send_keys(name_website)
         element.submit()
-        time.sleep(4)
+        time.sleep(10)
+        database.insert_new_url(name_website, driver.current_url)
         return driver.current_url
     finally:
         driver.close()
