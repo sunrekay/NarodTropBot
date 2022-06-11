49e378db9b27839da3f349a9dc460e507b08fc52
sunrekay <73135012+sunrekay@users.noreply.github.com>
2022-06-11 11:36:01 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -5,22 +5,12 @@ from url_bot_getter import get_website_url
 
 def url_data_check(name_website):
     try:
-        if "https://" in name_website:
-            response = requests.get(name_website)
+        temp = database.check_url_table(name_website)
+
+        if temp == 0:
+            return get_website_url(name_website)
         else:
-            name_website = f'https://{name_website}'
-            response = requests.get(name_website)
-
-        if response.status_code == 200:
-
-            temp = database.check_url_table(name_website)
-
-            if temp == 0:
-                return get_website_url(name_website)
-            else:
-                return temp
-        else:
-            return 0
+            return temp
     except Exception as ex:
         print(ex)
         return "Нет возможности получить доступ к сайту. Проверьте введенные данные."
